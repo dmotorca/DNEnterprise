dc2f6643fbed0188d39e6aeccc9c20ce199d63df
David Motorca <dmotorca@pdx.edu>
2025-09-30 18:33:09 -0700

    Testing

---
+++ example.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.play import play
+from test import get_arrivals
+
+import os
+
+load_dotenv()
+
+elevenlabs = ElevenLabs(
+  api_key=os.getenv("ELEVENLABS_API_KEY"),
+)
+
+audio = elevenlabs.text_to_speech.convert(
+    text="NICO IS REALLY AWESOME AND LOVES TRIMET",
+    voice_id="JBFqnCBsd6RMkjVDRZzb",
+    model_id="eleven_multilingual_v2",
+    output_format="mp3_44100_128",
+)
+
+
+play(audio)
+
