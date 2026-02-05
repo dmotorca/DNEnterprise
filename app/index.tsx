3d85d0eb58714674515891020be8ce6f9b098563
David <davidmotorca@gmail.com>
2026-02-04 18:21:24 -0800

    Pushing a new directory

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>If you see this it means that we finally locking into our App</Text>
+    </View>
+  );
+}
