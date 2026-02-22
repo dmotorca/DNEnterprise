5ec26997f008fea7aca2cb63eefe02d710cd27f0
David Motorca <davidmotorca@gmail.com>
2026-02-22 13:08:52 -0800

    first commit to my branch with beta maps function

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="map" options={{ title: 'Map' }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
+  );
+}
